19f21ffe20478b4215dee775d75ce66737e9b76f
chris artrip <christopherartrip@gmail.com>
2021-01-20 14:50:20 -0500

    first commit

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,9 @@
+const User = require('../../models/User');
+module.exports = {
+    Mutation: {
+        //args is RegisterInput from typeDefs.js
+        register(_, args, context, info) {
+            
+        }
+    }
+}
